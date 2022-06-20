ed8a25dc444459c6d9fbf685365a0194e1cbef0f
StecLukasz <lukaszstec14@gmail.com>
2022-06-20 16:49:45 +0200

    dodanie stylu css i dodawania eventu

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,14 @@
-console.log("test123");
+// <li>
+//   <label for="task-1">Wyprowadzić psa</label>
+//   <inputt type="checkbox" id="task-1" name="Wyprowadzić psa" />
+// </li>;
+const tasksContainerElement = document.querySelector(".tasks");
+const tasks = ["Wyprowadzić psa", "Iść na siłke"];
+const render = () => {
+    tasks.forEach((task) => {
+        const taskElement = document.createElement("li");
+        taskElement.innerText = task;
+        tasksContainerElement.appendChild(taskElement);
+    });
+};
+render();
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,29 @@
-console.log("test123");
+// <li>
+//   <label for="task-1">Wyprowadzić psa</label>
+//   <inputt type="checkbox" id="task-1" name="Wyprowadzić psa" />
+// </li>;
+
+const taskNameInputElement: HTMLInputElement = document.querySelector("@name");
+const addButtonElement: HTMLButtonElement = document.querySelector("button");
+
+const tasksContainerElement: HTMLElement = document.querySelector(".tasks");
+
+const tasks: string[] = ["Wyprowadzić psa", "Iść na siłke"];
+
+const render = () => {
+  tasks.forEach((task) => {
+    const taskElement: HTMLElement = document.createElement("li");
+    taskElement.innerText = task;
+    tasksContainerElement.appendChild(taskElement);
+  });
+};
+
+const addTask=(task: string) => {
+    tasks.push(task);
+}
+addButtonElement.addEventListener("click", (event: Event)=>{
+event.preventDefault();
+})
+
+
+render();
